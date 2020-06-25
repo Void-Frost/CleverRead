720f5e221c26231b71102e614d9c42d2c7011b13
Void-Frost <49461342+Void-Frost@users.noreply.github.com>
2020-06-24 21:47:59 -0500

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,11 +1,11 @@
 function AboutUs() {
-    window.location.href= "/AboutUs.html"
+    window.location.href= "/CleverRead/AboutUs.html"
 };
 function GoHome(){
-    window.location.href = "/index.html"
+    window.location.href = "/CleverRead/index.html"
 };
 function RandomArticle() {
     var articlenums = 1;
     var whicharticle = Math.floor(Math.random() * articlenums + 1);
-    window.location.href = "/Articles/Article" + whicharticle + ".html";
+    window.location.href = "/CleverRead/Articles/Article" + whicharticle + ".html";
 };
